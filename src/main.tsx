3ca35950f1782b335d0cd9b141a03aeec87b794c
kafumbatamaxie-oss <kafumbatamaxie@gmail.com>
2025-12-31 14:49:34 +0200

    Debug: minimal mount test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <div style={{ padding: 40, fontSize: 24 }}>
+    🚀 TeesNgees is mounting correctly
+  </div>
+);
